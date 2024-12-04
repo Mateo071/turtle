ddf7a8f30f52edcddd290d7224782caa20c85ac0
Mateo071 <floresmateo2000@gmail.com>
2024-12-04 12:47:50 -0500

    working challenge 3

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 from turtle import Turtle, Screen
+from random import randint
 
-ellen =  Turtle()
-ellen.shape("turtle")
-ellen.fillcolor("green")
-ellen.pencolor("brown")
+square =  Turtle()
+square.shape("circle")
+square.fillcolor("green")
+square.pencolor("brown")
 
+dotted_line = Turtle()
+dotted_line.shape("square")
+dotted_line.color("blue")
+
+diff_shapes = Turtle()
+diff_shapes.shape("circle")
+
+square.up()
+square.goto(-500, 45)
+square.down()
 for sides in range(4):
-  ellen.forward(90)
-  ellen.right(90)
+  square.forward(90)
+  square.right(90)
 
+dotted_line.up()
+dotted_line.goto(-100,-100)
+dotted_line.down()
+dotted_line.left(45)
+for step in range(20):
+  dotted_line.forward(10)
+  dotted_line.up()
+  dotted_line.forward(10)
+  dotted_line.down()
+
+diff_shapes.up()
+diff_shapes.goto(100, 200)
+diff_shapes.down()
+for shapes in range(3, 11):
+  # diff_shapes.pencolor((randint(0, 255), randint(0, 255), randint(0, 255)))
+  for shape in range(shapes):
+    diff_shapes.forward(100)
+    diff_shapes.right(360/shapes)
 
 screen = Screen()
 screen.exitonclick()

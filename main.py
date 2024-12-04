f2a8c11f2159da6c547134d7587a4b4093b810d5
Mateo071 <floresmateo2000@gmail.com>
2024-12-04 12:58:03 -0500

    randomized colors to third section

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ dotted_line.color("blue")
 diff_shapes = Turtle()
 diff_shapes.shape("circle")
 
+colors = ["black", "slate gray", "alice blue", "cornflower blue", "blue", "powder blue", "teal", "lime", "dark red", "yellow", "dark orange", "indian red", "medium slate blue", "deep pink"]
+
 square.up()
 square.goto(-500, 45)
 square.down()
@@ -34,7 +36,7 @@ diff_shapes.up()
 diff_shapes.goto(100, 200)
 diff_shapes.down()
 for shapes in range(3, 11):
-  # diff_shapes.pencolor((randint(0, 255), randint(0, 255), randint(0, 255)))
+  diff_shapes.pencolor(colors[randint(0, len(colors) - 1)])
   for shape in range(shapes):
     diff_shapes.forward(100)
     diff_shapes.right(360/shapes)

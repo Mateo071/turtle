7f48a2ecb634a2ae146cd1a6ad4fd2f1e558a3df
Mateo071 <floresmateo2000@gmail.com>
2024-12-04 13:34:59 -0500

    adjust module to allow for true random coloring

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import turtle
 from turtle import Turtle, Screen
-from random import randint
+from random import choice, randint
+turtle.colormode(255)
 
 square =  Turtle()
-square.shape("circle")
+square.shape("turtle")
 square.fillcolor("green")
 square.pencolor("brown")
 
@@ -13,7 +15,10 @@ dotted_line.color("blue")
 diff_shapes = Turtle()
 diff_shapes.shape("circle")
 
-colors = ["black", "slate gray", "alice blue", "cornflower blue", "blue", "powder blue", "teal", "lime", "dark red", "yellow", "dark orange", "indian red", "medium slate blue", "deep pink"]
+walk = Turtle()
+walk.shape("arrow")
+
+directions = [0, 90, 180, 270]
 
 square.up()
 square.goto(-500, 45)
@@ -23,7 +28,7 @@ for sides in range(4):
   square.right(90)
 
 dotted_line.up()
-dotted_line.goto(-100,-100)
+dotted_line.goto(-350,-100)
 dotted_line.down()
 dotted_line.left(45)
 for step in range(20):
@@ -33,13 +38,23 @@ for step in range(20):
   dotted_line.down()
 
 diff_shapes.up()
-diff_shapes.goto(100, 200)
+diff_shapes.goto(0, 200)
 diff_shapes.down()
 for shapes in range(3, 11):
-  diff_shapes.pencolor(colors[randint(0, len(colors) - 1)])
+  diff_shapes.pencolor((randint(0, 255), randint(0, 255), randint(0,255)))
   for shape in range(shapes):
     diff_shapes.forward(100)
     diff_shapes.right(360/shapes)
+
+walk.up()
+walk.goto(250, 0)
+walk.down()
+walk.speed(8)
+walk.pensize(5)
+for step in range(200):
+  walk.pencolor((randint(0, 255), randint(0, 255), randint(0,255)))
+  walk.setheading(choice(directions))
+  walk.forward(10)
 
 screen = Screen()
 screen.exitonclick()
